d469a3a3f64e42702101512964a92e00009a223c
ZollinoAndrea <ZollinoAndrea@gmail.com>
2024-03-14 09:17:22 +0100

    verdione 1:corretta

--- ZolllinoAndreaClass/src/Verifica.java
+++ ZolllinoAndreaClass/src/Verifica.java
@@ -1,5 +1,5 @@
 /**
- * F
+ * 
  * @author zollino andrea
  * <U> IV AI</U>
  *<C>14|03|2024</C>
@@ -18,11 +18,14 @@ public class Verifica {
 	 * la classe ZollinoMet consiscte in un calcolatore di produttoria di un certo range di numeri
 	 * 
 	 */
+	/* il problema si trova nel fatto che non è r=0 oppure non moltiplicherà mai;
+	 * nel for al posto di deve partire da s e finire a f-1;
+	 * al posto di r=r+r bisogna fare r=r*i;
+	*/
 	public int ZollinoMet(int s, int f) {
-		int r = 0;
-		
-		for (int i = 1; i < f; i++) {
-			r = r + r;
+		int r = 1;
+		for (int i = s; i < f+1; i++) {
+			r=r*i;
 		}
 		return r;
 	}
@@ -31,6 +34,6 @@ public class Verifica {
  */
 	public static void main(String[] args) {
 		Verifica q= new Verifica();
-		System.out.println(q.ZollinoMet(2,3));
+		System.out.println(q.ZollinoMet(5,7));
 	}
 }
